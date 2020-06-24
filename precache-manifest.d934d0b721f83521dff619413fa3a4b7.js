aca05573d524808694876883137e6b92b1ed7008
seancoleman2 <smcoleman27@gmail.com>
2020-06-23 17:24:13 -0700

    Updates

---
+++ precache-manifest.d934d0b721f83521dff619413fa3a4b7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d171f1b877ec48208ce54725859a00",
+    "url": "/seancoleman2/SimpleOKRs/index.html"
+  },
+  {
+    "revision": "2e8fb7a508de1d07af6d",
+    "url": "/seancoleman2/SimpleOKRs/static/css/2.afd40c00.chunk.css"
+  },
+  {
+    "revision": "80af863f748a9f27f12a",
+    "url": "/seancoleman2/SimpleOKRs/static/css/main.b09895df.chunk.css"
+  },
+  {
+    "revision": "2e8fb7a508de1d07af6d",
+    "url": "/seancoleman2/SimpleOKRs/static/js/2.323e43ce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/seancoleman2/SimpleOKRs/static/js/2.323e43ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80af863f748a9f27f12a",
+    "url": "/seancoleman2/SimpleOKRs/static/js/main.fd8bf7f0.chunk.js"
+  },
+  {
+    "revision": "764acb6b6f5d6c282c7a",
+    "url": "/seancoleman2/SimpleOKRs/static/js/runtime-main.cca4ec16.js"
+  },
+  {
+    "revision": "e969cf532ce2358a82f1ea6c3f1c0e98",
+    "url": "/seancoleman2/SimpleOKRs/static/media/add.e969cf53.png"
+  },
+  {
+    "revision": "a04f3879665a7972ea938531a70f80cc",
+    "url": "/seancoleman2/SimpleOKRs/static/media/list.a04f3879.png"
+  },
+  {
+    "revision": "0fbd1a689f405c469102e80ddae93000",
+    "url": "/seancoleman2/SimpleOKRs/static/media/progress-over-time.0fbd1a68.png"
+  },
+  {
+    "revision": "c2e0ffee123f1d77cd924ee0f672a57e",
+    "url": "/seancoleman2/SimpleOKRs/static/media/progress-reminder.c2e0ffee.png"
+  },
+  {
+    "revision": "6d8319da750da85989ecbaf92c747950",
+    "url": "/seancoleman2/SimpleOKRs/static/media/report-progress.6d8319da.png"
+  }
+]);
